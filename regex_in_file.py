84dd6dea763431ef79e679fa699dde1e166d4602
livnekes <livne.kestin@forter.com>
2019-11-24 10:31:47 +0200

    added script to match regex in file

---
+++ regex_in_file.py
@@ -0,0 +1,19 @@
+import argparse
+import re
+
+def main():
+    parser = argparse.ArgumentParser("find regex in file")
+    parser.add_argument("-f", dest = "file", help = "The name of the file")
+    parser.add_argument("-r", dest = "regex_str", help = "The regex")
+    args = parser.parse_args()
+
+    regex = r"\b(?=\w)" + re.escape(args.regex_str) + r"\b(?!\w)"
+    
+    f = open(args.file, "r")
+    fl = f.readlines()
+    for x in fl:
+        if re.search(regex, x):
+            print x
+if __name__== "__main__":
+    #python test.py -f cube_solver.py
+    main()
